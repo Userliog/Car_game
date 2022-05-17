bc0d412aeb4dc65f9f072d481661b04f3e37eb14
1 <liog04001@utb.vaxjo.se>
2022-05-17 15:53:13 +0200

    Created a script to change the lighting in a given scene based on two onscreen buttons

--- Assets/ChangeOptionsScript.cs
+++ Assets/ChangeOptionsScript.cs
@@ -5,28 +5,39 @@ using UnityEngine;
 public class ChangeOptionsScript : MonoBehaviour
 {
     [SerializeField] private ScriptableObject[] ScriptableObjectsLights;
+    [SerializeField] public Transform LightsContainer;
+    private int currentTimeIndex;
+    private int currentIndex;
 
     private void Awake()
     {
-        TimeChanger(0);
+        Transform checkpointsTransform = transform.Find("Checkpoints");
+
+        List<GameObject> Lights = new List<GameObject>();
+        foreach (Transform Light in LightsContainer)
+        {
+            Lights.Add(LightsContainer.GetComponentsInChildren);
+        }
+        TimeChanger(1);
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void TimeChanger(int change)
     {
-        currentTimeIndex += change;
-        if (currentIndex < 0)
+        currentIndex += change;
+        if (currentIndex > 0)
         {
-            currentIndex = ScriptableObjectsLights.Length - 1;
+            Lights<currentIndex>.SetActive(true);
+            currentIndex++;
         }
         else if (currentIndex > ScriptableObjectsLights.Length - 1)
         {
